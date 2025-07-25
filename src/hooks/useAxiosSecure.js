f587614f27b38c321f42007fee18051fc639e847
Millat Sarker <millatsarker224@gmail.com>
2025-07-26 02:35:53 +0600

    added the nineth code

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,32 +1,9 @@
-// import { useEffect } from 'react';
-// import axiosSecure from '../api/axiosSecure';
-
-// const useAxiosSecure = () => {
-//   useEffect(() => {
-//     // Optional: Add interceptor if you use token
-//     // axiosSecure.interceptors.request.use((config) => {
-//     //   const token = localStorage.getItem('access-token');
-//     //   if (token) {
-//     //     config.headers.Authorization = `Bearer ${token}`;
-//     //   }
-//     //   return config;
-//     // });
-//   }, []);
-
-//   return axiosSecure;
-// };
-
-// export default useAxiosSecure;
-
 import { useEffect } from 'react';
 import axiosSecure from '../api/axiosSecure';
 
-/**
- * Custom hook to return secured Axios instance with token interceptor.
- */
 const useAxiosSecure = () => {
   useEffect(() => {
-    // Add request interceptor to attach token
+
     const interceptor = axiosSecure.interceptors.request.use((config) => {
       const token = localStorage.getItem('access-token');
       if (token) {
@@ -35,7 +12,6 @@ const useAxiosSecure = () => {
       return config;
     });
 
-    // Remove interceptor on component unmount
     return () => {
       axiosSecure.interceptors.request.eject(interceptor);
     };
