e5e4ce818970573237aeaf212238c78234a7fc54
Millat Sarker <millatsarker224@gmail.com>
2025-10-20 19:22:05 +0600

    added the job code

--- src/dashboard/MemberDashboard/ConfirmedBookings.jsx
+++ src/dashboard/MemberDashboard/ConfirmedBookings.jsx
@@ -7,7 +7,7 @@ function ConfirmedBookings() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:5000/bookings/confirmed?page=1&limit=10')
+        axios.get('https://my-assignment-12-server-kappa.vercel.app/bookings/confirmed?page=1&limit=10')
             .then(res => {
                 const data = res.data;
                 if (data && data.bookings) {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,12 +26,12 @@ const Login = () => {
       const email = result.user.email;
 
       // Token from server
-      const jwtRes = await axios.post("http://localhost:5000/jwt", { email });
+      const jwtRes = await axios.post("https://my-assignment-12-server-kappa.vercel.app/jwt", { email });
       const token = jwtRes.data.token;
       localStorage.setItem("access-token", token);
 
       // Get role from server
-      const roleRes = await axios.get(`http://localhost:5000/users/role/${email}`, {
+      const roleRes = await axios.get(`https://my-assignment-12-server-kappa.vercel.app/users/role/${email}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
@@ -57,12 +57,12 @@ const Login = () => {
       const result = await signInWithEmailAndPassword(auth, email, password);
       toast.success("Login successful");
 
-      const jwtRes = await axios.post("http://localhost:5000/jwt", { email });
+      const jwtRes = await axios.post("https://my-assignment-12-server-kappa.vercel.app/jwt", { email });
       const token = jwtRes.data.token;
       localStorage.setItem("access-token", token);
 
-      // const roleRes = await axios.get(`http://localhost:5000users/role/${email}`, {
-      const roleRes = await axios.get(`http://localhost:5000/users/role/${email}`, {
+      // const roleRes = await axios.get(`https://my-assignment-12-server-kappa.vercel.appusers/role/${email}`, {
+      const roleRes = await axios.get(`https://my-assignment-12-server-kappa.vercel.app/users/role/${email}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
